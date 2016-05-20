f99e98aa88e12869d534d8a3af8878fd05e05200
Phildo <Phildo211@gmail.com>
2016-05-20 15:45:17 -0500

    merged in cycle dom

--- src/dom.js
+++ src/dom.js
@@ -60,6 +60,7 @@ var CanvDom = function(canv)
   var m;
   var c;
 
+  self.dismissing = false;
   self.x = 0;
   self.y = 0;
   self.w = 0;
@@ -67,18 +68,24 @@ var CanvDom = function(canv)
 
   var mclicked = function(evt)
   {
+    self.dismissing = true;
     m = undefined;
     if(c) c();
-    c = undefined;
+    if(self.dismissing) //if still dismissing (callback didn't pop another...)
+    {
+      c = undefined;
 
-    self.x = 0;
-    self.y = 0;
-    self.w = 0;
-    self.h = 0;
+      self.x = 0;
+      self.y = 0;
+      self.w = 0;
+      self.h = 0;
+    }
+    self.dismissing = false;
   }
 
   self.popDismissableMessage = function(text,x,y,w,h,callback)
   {
+    self.dismissing = false;
     if(m) mclicked();
     m = text;
     c = callback;
@@ -91,15 +98,14 @@ var CanvDom = function(canv)
   }
 
   self.click = mclicked;
-  self.draw = function(canv)
+  self.draw = function(text_h,canv)
   {
-    if(m)
+    canv.context.fillStyle = "#000000";
+    canv.context.textAlign = "left";
+    if(!Array.isArray(m)) m = [m]; //boy I'm lazy...
+    for(var i = 0; i < m.length; i++)
     {
-      canv.context.fillStyle = "#FF0000";
-      canv.context.fillRect(self.x,self.y,self.w,self.h);
-      canv.context.fillStyle = "#000000";
-      canv.context.textAlign = "left";
-      canv.context.fillText(m,self.x,self.y+self.h);
+      if(m[i]) canv.context.fillText(m[i],self.x,self.y+(text_h+2)*(i+1));
     }
   }
 }
