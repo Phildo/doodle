48fac091f86b68abc13ace49e9df6b74b4510eb0
Phildo <phildo211@gmail.com>
2018-08-07 10:08:13 -0500

    merged capitol load

--- src/scenes/loadingScene.js
+++ src/scenes/loadingScene.js
@@ -11,6 +11,8 @@ var LoadingScene = function(game, stage)
     canv = stage.canv;
     canvas = canv.canvas;
     ctx = canv.context;
+
+    //ctx.font = text_font;
   }
   self.resize(stage);
 
@@ -32,6 +34,12 @@ var LoadingScene = function(game, stage)
   var n_imgs_loaded;
   var img_srcs;
   var imgs;
+  var n_fonts_loaded;
+  var font_srcs;
+  var font_loaded;
+  var font_wrongdata;
+  var font_canv;
+  var font_canv_s;
   var n_audios_loaded;
   var audio_srcs;
   var audios;
@@ -44,6 +52,10 @@ var LoadingScene = function(game, stage)
   {
     n_imgs_loaded++;
   };
+  var fontLoaded = function()
+  {
+    n_fonts_loaded++;
+  };
   var audioLoaded = function()
   {
     n_audios_loaded++;
@@ -54,6 +66,14 @@ var LoadingScene = function(game, stage)
     canv = stage.canv;
     canvas = canv.canvas;
     ctx = canv.context;
+  }
+
+  var tryfont = function()
+  {
+    font_canv.context.fillStyle = "#FFFFFF";
+    font_canv.context.fillRect(0,0,font_canv_s,font_canv_s);
+    font_canv.context.fillStyle = "#000000";
+    font_canv.context.fillText("a",12,22);
   }
 
   self.ready = function()
@@ -76,16 +96,15 @@ var LoadingScene = function(game, stage)
     n_imgs_loaded = 0;
     img_srcs = [];
     imgs = [];
+    n_fonts_loaded = 0;
+    font_srcs = [];
+    font_loaded = [];
     n_audios_loaded = 0;
     audio_srcs = [];
     audios = [];
 
-    //ctx.font = "12px SpecialFont"; //put font that nees loading here
-    ctx.fillStyle = "#000000";
-    ctx.fillText(".",0,0);// funky way to encourage any custom font to load
-
     //put asset paths in loading_img_srcs (for assets used on loading screen itself)
-    //loading_img_srcs.push("assets/man.png");
+    //loading_img_srcs.push("assets/loading_img.png");
     for(var i = 0; i < loading_img_srcs.length; i++)
     {
       loading_imgs[i] = new Image();
@@ -95,7 +114,7 @@ var LoadingScene = function(game, stage)
     loadingImageLoaded(); //call once to prevent 0/0 != 100% bug
 
     //put asset paths in img_srcs
-    //img_srcs.push("assets/man.png");
+    //img_srcs.push("assets/img.jpg");
     for(var i = 0; i < img_srcs.length; i++)
     {
       imgs[i] = new Image();
@@ -104,8 +123,25 @@ var LoadingScene = function(game, stage)
     }
     imageLoaded(); //call once to prevent 0/0 != 100% bug
 
+    //put font paths in font_srcs
+    //font_srcs.push("FontName"); //NEEDS DEFINITION IN index.html CSS!!
+    font_canv_s = 25;
+    font_canv = GenIcon(font_canv_s,font_canv_s);
+    // uncomment below for preview
+    //font_canv.style.position="absolute";
+    //font_canv.style.left="-"+font_canv_s+"px";
+    //document.getElementById("content").appendChild(font_canv);
+    font_canv.context.font = "20px THISFONTDOESNTEXIST";
+    tryfont();
+    font_wrongdata = font_canv.context.getImageData(0,0,font_canv_s,font_canv_s);
+    for(var i = 0; i < font_srcs.length; i++)
+    {
+      font_loaded[i] = 0;
+    }
+    fontLoaded(); //call once to prevent 0/0 != 100% bug
+
     //put asset paths in audio_srcs
-    //audio_srcs.push("assets/sound.mp3");
+    //audio_srcs.push("assets/audio.mp3");
     for(var i = 0; i < audio_srcs.length; i++)
     {
       audios[i] = new Audio();
@@ -117,31 +153,90 @@ var LoadingScene = function(game, stage)
 
   self.tick = function()
   {
+    //font main-thread loaded test
+    for(var i = 0; i < font_srcs.length; i++)
+    {
+      var font_data;
+      if(!font_loaded[i])
+      {
+        font_canv.context.font = "20px "+font_srcs[i];
+        tryfont();
+        font_data = font_canv.context.getImageData(0,0,font_canv_s,font_canv_s);
+        var j;
+        for(j = 0; font_data.data[j] == font_wrongdata.data[j] && j < font_data.data.length; j++) ;
+        if(j < font_wrongdata.data.length)
+        {
+          fontLoaded();
+          ctx.font = "20px "+font_srcs[i];
+          ctx.fillText(".",0,0);
+          font_loaded[i] = 1;
+        }
+      }
+    }
+
     //note- assets used on loading screen itself NOT included in wait
     loading_percent_loaded = n_loading_imgs_loaded/(loading_img_srcs.length+1);
     if(loading_percent_loaded >= 1.0) ticks_since_loading_ready++;
-    percent_loaded = (n_imgs_loaded+n_audios_loaded)/((img_srcs.length+1)+(audio_srcs.length+1));
+    percent_loaded = (n_imgs_loaded+n_fonts_loaded+n_audios_loaded)/((img_srcs.length+1)+(font_srcs.length+1)+(audio_srcs.length+1));
     if(chase_percent_loaded <= percent_loaded) chase_percent_loaded += 0.01;
     lerp_percent_loaded = lerp(lerp_percent_loaded,percent_loaded,0.1);
     lerp_chase_percent_loaded = lerp(lerp_chase_percent_loaded,chase_percent_loaded,0.1);
     if(percent_loaded >= 1.0) ticks_since_ready++;
     if(ticks_since_ready >= post_load_countdown)
     {
-      //any last minute preparation
-      game.nextScene();
+      if(ticks_since_loading_ready > 1) game.nextScene(); //set 1 = # ticks in preloading sequence
+      //game.nextScene();
     }
   };
 
-
   self.draw = function()
   {
-    ctx.fillRect(pad,canv.height/2,chase_percent_loaded*barw,1);
-    ctx.strokeRect(pad-1,(canv.height/2)-1,barw+2,3);
+    if(chase_percent_loaded < 1)
+    {
+      ctx.fillStyle = "#888888";
+      ctx.strokeStyle = "#888888";
+      ctx.fillRect(pad,canv.height/2,chase_percent_loaded*barw,1);
+      ctx.strokeRect(pad-1,(canv.height/2)-1,barw+2,3);
+    }
 
     if(loading_percent_loaded >= 1)
     {
-      //do any special drawing here
+      //do any special drawing here- use 'ticks_since_loading_ready' as t for preloading sequence
     }
+
+/*
+    ctx.fillStyle = blue;
+    var x = 10;
+    var y = 30;
+    for(var i = 0; i < loading_img_srcs.length; i++)
+    {
+      ctx.fillText(loading_img_srcs[i],x,y);
+      y += 20;
+    }
+    ctx.fillText(n_loading_imgs_loaded,x,y);
+    y += 20;
+    for(var i = 0; i < img_srcs.length; i++)
+    {
+      ctx.fillText(img_srcs[i],x,y);
+      y += 20;
+    }
+    ctx.fillText(n_imgs_loaded,x,y);
+    y += 20;
+    for(var i = 0; i < font_srcs.length; i++)
+    {
+      ctx.fillText(font_srcs[i],x,y);
+      y += 20;
+    }
+    ctx.fillText(n_fonts_loaded,x,y);
+    y += 20;
+    for(var i = 0; i < audio_srcs.length; i++)
+    {
+      ctx.fillText(audio_srcs[i],x,y);
+      y += 20;
+    }
+    ctx.fillText(n_audios_loaded,x,y);
+    y += 20;
+*/
   };
 
   self.cleanup = function()
@@ -151,3 +246,4 @@ var LoadingScene = function(game, stage)
     loading_imgs = [];//just used them to cache assets in browser; let garbage collector handle 'em.
   };
 };
+
